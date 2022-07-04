6a5cc3e3e83f05b8ed4c4847036a713f8d9cfcbb
Alessandro Chitolina <alekitto@gmail.com>
2022-07-04 17:15:55 +0200

    remove spanbuilder, add span finish and toTraceparent methods

--- src/TracerInterface.php
+++ src/TracerInterface.php
@@ -1,8 +1,19 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Psr\Tracing;
 
+/**
+ * A Tracer object should provide methods to create spans and handle context and
+ * traces. A trace is a collection of spans.
+ */
 interface TracerInterface
 {
-    public function spanBuilder(string $spanName): SpanBuilderInterface;
+    /**
+     * Creates and set the start timestamp of a new Span.
+     * This will NOT set the newly created span as the "current" span:
+     * {@see SpanInterface::activate} should be called.
+     */
+    public function startSpan(string $spanName): SpanInterface;
 }
